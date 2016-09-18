49ddda17dfb66e58942710285d01f8d43e94c060
StergiosCha <kafouroutsos@hotmail.com>
2016-09-18 21:42:46 +0200

    Update MontagovianLexiconToy.v

--- MontagovianLexiconToy.v
+++ MontagovianLexiconToy.v
@@ -1,5 +1,6 @@
-
 (** Retore's 2013 toy semantic grammar including multi-sortedness and coercions to handle co-predication*)
+Load LibTactics.
+Ltac AUTO:= cbv delta;intuition;try repeat congruence;  jauto;intuition.
 Definition e:=Set.
 Parameters Physical_Object Informational_Object Human  People Place Book Town:e.
 Let H:= Human.
@@ -30,12 +31,14 @@ Parameter read: Human->I->Prop.
 Parameter mastered: Human->I->Prop.
 Parameter picked_up:Human->Phi->Prop.
 Parameter ksi:e.
+Unset Implicit Arguments. 
 Definition PAND:= fun a:e=>fun b: e=> fun P:a->t=>fun Q:b->t=>fun x:e=>fun y:x=>fun f: x->a=>fun g:x->b=> and(P(f(y)))(Q(g(y))).
 Parameter John:H. 
 Parameter War_and_Peace: B .
 Check b4.
 Check mastered John (t2  War_and_Peace).
 Check picked_up John (t1 War_and_Peace).
+Print PAND. 
 Check PAND Phi I (picked_up John)(mastered  John)B War_and_Peace (t1)(t2).
-Theorem sdfsd: PAND Phi I (picked_up John)(mastered  John)B War_and_Peace (t1)(t2)-> and (picked_up John (t1 War_and_Peace))(mastered John(t2 War_and_Peace)).
+Theorem PAND1: PAND Phi I (picked_up John)(mastered  John)B War_and_Peace (t1)(t2)-> and (picked_up John (t1 War_and_Peace))(mastered John(t2 War_and_Peace)). (**AUTO fully automates the proof*)
 cbv. intro. assumption. Qed. 
